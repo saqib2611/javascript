c4194b87956e1898fb81fe6d9c1c897793e46765
Engr Saqib Jameel <65038331+saqib2611@users.noreply.github.com>
2020-06-14 19:51:29 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,142 @@
+// chapter 1 Alert 
+// 1.
+//alert("Good Mornig!")
+//2.
+//alert("Error! Please Enter a valid password")
+
+//3.
+//alert("Welcome to Js Land...\nHappy Coding!")
+
+//4.
+//alert("Welcome to Js Land...")
+//alert("Happy Coding!\nPrevent this page from creating additionsl dialogsbox")
+ 
+//5.
+//var a ="Hello...I can run Js through my Web Browser's Console";
+//console.log(a)
+//alert(a)
+
+//6.
+
+
+
+
+
+
+
+
+//chapter 2 varible for string
+
+//1.
+// var username;
+
+//2.
+// var myname;
+// myname='Saqib jameel';
+// console.log(myname);
+
+//3.
+
+// let message;
+// message = 'Hellow world';
+// alert(message)
+
+//4.
+
+// var user = "saqib Jameel";
+// var age = "22 year";
+// var course = "Certified Mobile Appliction Development";
+// alert(user);
+// alert(age);
+// alert(course);
+
+//6.
+
+// var message;
+// message='my email address is, ';
+// var email;
+// email = 'someone@someone.com';
+// var add;
+// add = message + '' + email;
+// document.write(add);
+// alert(add)
+
+//5.
+
+// var i;
+// var j;
+// for(i=1; i < 5; i++) {
+
+//   for(j=1; j<=i; j--) {
+//      document.write('#');
+// }
+// document.write('<br/>');
+// }
+
+//7.
+// var message;
+// message= 'I am trying to learn from the Book ';
+// var book;
+// book =  ' A smarter way to learn JavaScript.';
+// var c =(message + book);
+// alert(c)
+
+//8.
+
+// var a;
+// a = 'Yah! I can Write HTML content through JavaScript ';
+// document.write(a);
+
+//9.
+
+
+
+
+
+
+
+// chapter 3 varible for number
+
+//1.
+
+// var age;
+// age = '22 year';
+// alert(age);
+
+//2.
+
+
+
+
+
+//3.
+
+// var message;
+// message = 'My Brithday Year is ';
+// var brithyear;
+// brithyear = 2000;
+// document.write(message);
+// document.write(brithyear);
+
+//4.
+
+// var VistorsName ="John Doe";
+// var producttitle =" T-shirt ";
+// var Quantity = 5 ;
+// a = " Ordered ";
+// b ='(s) on XYZ Clothing store ';
+// document.write(VistorsName);
+// document.write(a);
+// document.write(Quantity);
+// document.write(producttitle);
+// document.write(b)
+
+// chapter 4 legal and iilegal 
+
+
+
+
+
+
+
+
